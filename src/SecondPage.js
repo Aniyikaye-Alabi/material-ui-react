06832af812182c0b5ee0e35edb907f38a52579bd
Aniyikaye Alabi <adalabi@sidmach.com>
2021-09-10 17:36:38 +0100

    Added dependency array and toggle button

--- src/SecondPage.js
+++ src/SecondPage.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const SecondPage = () => {
     const arr = ["ad", "be", "cj"]
+    const [name, setName] = useState("mario");
+
+    useEffect(() => {
+        console.log("effect ran");
+    }, [name]);
+
+    let condName = (name === "mario") ? "fish" : "mario";
     return (
         <div>
             <h1>Hello Welcome to My Second Page</h1>
             {
                 arr.map((each, index) => <li key={index}>{each}</li>)
             }
+            <p>{name}</p>
+            <button onClick={() => {setName(condName)}}>
+            Toggle
+            </button>
         </div>
     );
 }
